e1a542452c8b193e183eb5522f113b3bb72f0052
Gunn Bhatrakarn <xyzxyz442@gmail.com>
2018-08-28 05:07:39 +0700

    Due to the recursion config was removed after version 4.1.0 (powerdns authoritative). Adding recursor service (from pschiffe/pdns-recursor) as main entry for resolve dns request and forward if dns not exist.

---
+++ docker/pdns-recursor/docker-cmd.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -euo pipefail
+
+# Configure base vars
+: "${PDNS_local_port:=53}"
+: "${PDNS_local_address:=0.0.0.0}"
+
+export PDNS_local_port PDNS_local_address
+
+# Create config file from template
+envtpl < /recursor.conf.tpl > /etc/pdns/recursor.conf
+
+# fix config right
+if [ getent passwd | grep -c '^pdns-recursor:' ]; then
+    # Fedora user
+    chown pdns-recursor:pdns-recursor /etc/pdns/recursor.conf
+else
+    # Alpine user
+    chown recursor:recursor /etc/pdns/recursor.conf
+fi
+
+exec /usr/sbin/pdns_recursor
